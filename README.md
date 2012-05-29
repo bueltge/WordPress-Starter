6438bd7ae4636c45930b53a7b7bac8d5a919f73c
Frank Bültge <frank@bueltge.de>
2012-05-29 21:00:39 +0200

    first init, first checkin of an simple skeleton for WP Dev plattforms

---
+++ README.md
@@ -0,0 +1,23 @@
+# WordPress Starter
+This is simple starter skeleton repo for a WordPress developer site - reduce the `wp-config.php` for live sites.
+
+## Description
+* `wpbeta` as default name for the wp-directory; why i ise the last nightly build for development
+* WordPress as a Git submodule in `/wpbeta/`
+* Custom content directory in `/wp-content/`
+* Custom plugin directory in `/wp-plugins/`
+* Custom multisite plugin directory in `/wpmu-plugins/`
+* `wp-config.php` in the root
+* `Default as Multisite mode
+
+## Other Notes
+### Licence
+Good news, this plugin is free for everyone! Since it's released under the GPL, you can use it free of charge on your personal or commercial blog. But if you enjoy this plugin, you can thank me and leave a [small donation](http://bueltge.de/wunschliste/ "Wishliste and Donate") for the time I've spent writing and supporting this plugin. And I really don't want to know how many hours of my life this plugin has already eaten ;)
+
+### Contact & Feedback
+The plugin is designed and developed by me ([Frank Bültge](http://bueltge.de))
+
+Please let me know if you like the plugin or you hate it or whatever ... Please fork it, add an issue for ideas and bugs.
+
+### Disclaimer
+I'm German and my English might be gruesome here and there. So please be patient with me and let me know of typos or grammatical farts. Thanks
---
+++ wp-config.php
@@ -0,0 +1,111 @@
+<?php
+//*
+// For Debug purpose
+@ini_set( 'log_errors', 'On' );
+@ini_set( 'display_errors', 'On' );
+@ini_set( 'error_log', 'http://' . $_SERVER['HTTP_HOST'] . '/logs/php_errors.' . date( 'Y-m-d' ) . '.log' );
+
+define( 'WP_DEBUG', TRUE );
+define( 'WP_DEBUG_DISPLAY', TRUE ); 
+define( 'WP_DEBUG_LOG', '/logs' );
+// Debug JS, When this is defined and set to TRUE the non-minified versions of the Javascripts will be used.
+define( 'SCRIPT_DEBUG', TRUE );
+/**/
+
+/*
+// for deactivate login/logout
+function wp_validate_auth_cookie($cookie = '', $scheme = '' ) {
+	
+	return 1;
+}
+/**/
+
+/*
+// Proxy Settings
+define( 'WP_PROXY_HOST', '10.28.123.4' );
+define( 'WP_PROXY_PORT', '8080' );
+define( 'WP_PROXY_USERNAME', 'username123' );
+define( 'WP_PROXY_PASSWORD', 'password123' );
+define( 'WP_PROXY_BYPASS_HOSTS', 'localhost' );
+/**/
+
+/*
+// FTP Data
+//define( 'FTP_HOST', '' );
+//define( 'FTP_USER', 'username123' );
+//define( 'FTP_PASS', 'password123' );
+/**/
+
+/*
+// MySQL settings
+define( 'DB_NAME', 'wpbeta' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+/**/
+
+//*
+// Custom content directory
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
+// Custom plugin directory
+define( 'WP_PLUGIN_DIR', dirname( __FILE__ ) . '/wp-plugins' );
+define( 'WP_PLUGIN_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-plugins' );
+// Custom mu plugin directory
+define( 'WPMU_PLUGIN_DIR', dirname( __FILE__ ) . '/wpmu-plugins' );
+define( 'WPMU_PLUGIN_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wpmu-plugins' );
+/**/
+
+// Set memory limit
+define( 'WP_MEMORY_LIMIT', '128M' );
+
+// Save queries for read adn optimize
+define( 'SAVEQUERIES', TRUE );
+
+// Set revisions
+define( 'WP_POST_REVISIONS', 3 );
+
+// Compression for JS and styles
+define( 'CONCATENATE_SCRIPTS', FALSE ); // activate concantenate scripts 
+define( 'COMPRESS_SCRIPTS', FALSE ); // activate JS compression
+
+// Allow Multisite
+define( 'WP_ALLOW_MULTISITE', TRUE );
+
+//*
+// Network setup
+define( 'MULTISITE',            TRUE );
+define( 'SUBDOMAIN_INSTALL',    FALSE );
+$base = '/wpbeta/';
+define( 'DOMAIN_CURRENT_SITE',  'localhost' );
+define( 'PATH_CURRENT_SITE',    '/wpbeta/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+/**/
+
+// Salts, for security
+// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+// Table prefix
+// Change this if you have multiple installs in the same database
+$table_prefix  = 'wpbeta_';
+
+// Language
+// Leave blank for American English
+define ( 'WPLANG', '' );
+
+// Bootstrap WordPress
+if ( ! defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wpbeta' );
+
+require_once( ABSPATH . 'wp-settings.php' );
