e3228268acaf8914c864d1dd0569d34342c085ef
Frank Bültge <frank@bueltge.de>
2012-05-30 10:15:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
-	if ( ! empty( $_SERVER['HTTPS'] ) && ( 'on' == $_SERVER['HTTPS'] ) ) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	
-	header( 'Location: ' . $uri . '/wpbeta/' );
-	exit;
+/**
+// Stop most of WordPress from being loaded if we just want the basics.
+define( 'SHORTINIT', TRUE );
+/**/
+
+// WordPress view bootstrapper
+define( 'WP_USE_THEMES', TRUE );
+// Loads the WordPress Environment and Template
+require( './wpbeta/wp-blog-header.php' );
+
+if ( ! empty( $_SERVER['HTTPS'] ) && ( 'on' == $_SERVER['HTTPS'] ) ) {
+	$uri = 'https://';
+} else {
+	$uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+
+header( 'Location: ' . $uri . '/wpbeta/' );
+exit;
